440c5a6964dd510689f8f00e054475ea69226aee
rickon <chenbiao@heomai.com>
2025-04-17 18:29:47 +0800

    版本号更新

--- src/Config/Blog.php
+++ src/Config/Blog.php
@@ -1,25 +1,25 @@
 <?php
 /**
- * 
+ *
  * This file is auto generate by Nicelizhi\Apps\Commands\Create
  * @author Steve
  * @date 2025-04-11 09:02:26
  * @link https://github.com/xxxl4
- * 
+ *
  */
 return [
     /**
-     * 
+     *
      * The name of the package
      */
     'name' => 'Blog',
     /**
-     * 
+     *
      * The version of the package
      */
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     /**
-     * 
+     *
      * The version number of the package
      */
     'versionNum' => '100',
@@ -36,55 +36,55 @@ return [
     'composer' => 'nexa-merchant/blog',
 
     /**
-     * 
+     *
      * The description of the package
      */
     'description' => '',
 
     /**
-     * 
+     *
      * The enable of the package
      */
     'enable' => env('Blog_ENABLE', true),
 
     /**
-     * 
+     *
      * The author of the package
      */
     'author' => 'Steve',
 
     /**
-     * 
+     *
      * The email of the author
      */
     'email' => 'email@example.com',
 
     /**
-     * 
+     *
      * The homepage of the package
      */
     'homepage' => 'https://github.com/xxl4',
 
     /**
-     * 
+     *
      * The keywords of the package
      */
     'keywords' => [],
 
     /**
-     * 
+     *
      * The license of the package
      */
     'license' => 'MIT',
 
     /**
-     * 
+     *
      * The type of the package
      */
     'type' => 'library',
 
     /**
-     * 
+     *
      * The support of the package
      */
     'support' => [
